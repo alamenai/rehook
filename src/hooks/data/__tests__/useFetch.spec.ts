5833ab627739ca1eacca90c98711883c5a43cfdd
Ala Eddine <ala.official.acc@gmail.com>
2023-11-04 21:53:21 +0100

    test: add unit test for useFetch hook

---
+++ src/hooks/data/__tests__/useFetch.spec.ts
@@ -0,0 +1,66 @@
+import { useFetch } from '../useFetch'
+import { renderHook, waitFor } from '@testing-library/react'
+
+test('useFetch should fetch data immediately', async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+
+    const { result } = renderHook(() => useFetch(url))
+
+    expect(result.current.data).toBe(undefined)
+
+    expect(result.current.error).toBe('')
+
+    await waitFor(() => expect(result.current.data).not.toBeUndefined())
+
+    await waitFor(() => expect(result.current.error).toBe(''))
+
+    await waitFor(() => expect(result.current.isLoading).toBe(false))
+})
+
+test('useFetch should fetch data immediately trigger is false', async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+
+    const { result } = renderHook(() => useFetch(url, false))
+
+    expect(result.current.data).toBe(undefined)
+
+    expect(result.current.error).toBe('')
+
+    await waitFor(() => {
+        expect(result.current.data).not.toBeUndefined()
+
+        expect(result.current.error).toBe('')
+
+        expect(result.current.isLoading).toBe(false)
+    })
+})
+
+test('useFetch should not fetch data immediately trigger is true', async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+
+    const { result } = renderHook(() => useFetch(url, true))
+
+    expect(result.current.data).toBeUndefined()
+
+    expect(result.current.error).toBe('')
+
+    expect(result.current.isLoading).toBe(false)
+})
+
+test('useFetch should handle errors', async () => {
+    const url = 'https://jsonplaceholder.typicode.com/wrong'
+
+    const { result } = renderHook(() => useFetch(url))
+
+    expect(result.current.data).toBe(undefined)
+
+    expect(result.current.error).toBe('')
+
+    await waitFor(() => {
+        expect(result.current.data).toBeUndefined()
+
+        expect(result.current.error).toBe('HTTP error! Status: 404')
+
+        expect(result.current.isLoading).toBe(false)
+    })
+})
