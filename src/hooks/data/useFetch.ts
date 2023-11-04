7f1bdce7252657fa26856572a1411035e8f84cfa
Ala Eddine <ala.official.acc@gmail.com>
2023-11-04 21:50:31 +0100

    feat: implement useFetch hook for fetching data

---
+++ src/hooks/data/useFetch.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+export const useFetch = (url: string, trigger?: boolean, requestOptions?: RequestInit) => {
+    const [error, setError] = useState('')
+
+    const [data, setData] = useState<any | undefined>(undefined)
+
+    const [isLoading, setLoading] = useState(!trigger || false)
+
+    const [onTrigger, _setTrigger] = useState(trigger)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url, requestOptions)
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+
+            const result = await response.json()
+
+            setData(result)
+
+            setError('')
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message)
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if (!onTrigger) {
+            fetchData()
+        }
+    }, [onTrigger])
+
+    return { data, isLoading, error, refetch: fetchData }
+}
