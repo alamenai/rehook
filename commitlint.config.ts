94b26639b4c1c66d42b05fb7127a6528226535ad
Ala Eddine <ala.official.acc@gmail.com>
2023-11-07 00:40:23 +0100

    update: add more type-enum for commits

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,11 +1,15 @@
-import type { UserConfig } from '@commitlint/types';
+import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
     extends: ['@commitlint/config-conventional'],
 
     rules: {
-        'type-enum': [2, 'always', ['config']],
+        'type-enum': [
+            2,
+            'always',
+            ['config', 'feat', 'test', 'del', 'refactor', 'style', 'deprecate', 'chore', 'ci', 'build', 'update'],
+        ],
     },
-};
+}
 
-module.exports = Configuration;
+module.exports = Configuration
