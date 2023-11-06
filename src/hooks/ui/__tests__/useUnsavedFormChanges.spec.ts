237bc0d39931fcbcf1de79c2b097f7874a1eb74e
Ala Eddine <ala.official.acc@gmail.com>
2023-11-06 21:19:57 +0100

    test: add unit test for useUnsavedFormChanges hook

---
+++ src/hooks/ui/__tests__/useUnsavedFormChanges.spec.ts
@@ -0,0 +1,36 @@
+import { useUnsavedFormChanges } from '../useUnsavedFormChanges'
+import { renderHook, act } from '@testing-library/react'
+
+describe('useUnsavedFormChanges', () => {
+    it('should initialize with isFormChanged as false', () => {
+        const { result } = renderHook(() => useUnsavedFormChanges())
+
+        expect(result.current.isFormChanged).toBe(false)
+    })
+
+    it('should set isFormChanged to true when calling setFormChanged', () => {
+        const { result } = renderHook(() => useUnsavedFormChanges())
+
+        act(() => {
+            result.current.setFormChanged(true)
+        })
+
+        expect(result.current.isFormChanged).toBe(true)
+    })
+
+    it('should add and remove event listener for beforeunload', () => {
+        const addEventListenerSpy = jest.spyOn(window, 'addEventListener')
+        const removeEventListenerSpy = jest.spyOn(window, 'removeEventListener')
+
+        const { unmount } = renderHook(() => useUnsavedFormChanges())
+
+        expect(addEventListenerSpy).toHaveBeenCalledWith('beforeunload', expect.any(Function))
+
+        unmount()
+
+        expect(removeEventListenerSpy).toHaveBeenCalledWith('beforeunload', expect.any(Function))
+
+        addEventListenerSpy.mockRestore()
+        removeEventListenerSpy.mockRestore()
+    })
+})
