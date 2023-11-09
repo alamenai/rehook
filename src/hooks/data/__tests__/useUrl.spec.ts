7e6b7c6e0d0be54b696182f01c69ce871559aaaa
Ala Eddine <ala.official.acc@gmail.com>
2023-11-09 20:09:36 +0100

    test: create unit test for useUrl hook

---
+++ src/hooks/data/__tests__/useUrl.spec.ts
@@ -0,0 +1,34 @@
+import { useUrl } from '../useUrl'
+import { renderHook } from '@testing-library/react'
+
+const OLD_LOCATION = window.location
+
+beforeEach(() => {
+    Object.defineProperty(window, 'location', {
+        value: new URL('http://localhost:5173/search?id=1&name=alaeddine'),
+        writable: true,
+    })
+})
+
+afterAll(() => {
+    Object.defineProperty(window, 'location', {
+        value: OLD_LOCATION,
+        writable: true,
+    })
+})
+
+test('should return the correct url, port and query params', () => {
+    const { result } = renderHook(() => useUrl())
+
+    expect(result.current.url).toBe(window.location.href)
+
+    expect(result.current.getPort()).toBe(window.location.port)
+
+    expect(result.current.getQueryParams()).toStrictEqual({ id: '1', name: 'alaeddine' })
+})
+
+test('should return the correct root pathname', () => {
+    const { result } = renderHook(() => useUrl())
+
+    expect(result.current.getRootPath()).toBe(window.location.pathname.split('/')[1])
+})
