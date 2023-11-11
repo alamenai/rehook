d5ac0f4caef430e4766c2497b7da59d60b5a2403
Menai Ala Eddine <ala.official.acc@gmail.com>
2023-11-11 20:36:30 +0100

    feat: Implement useLoading hook

---
+++ src/hooks/data/useLocalStorage.ts
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+
+export const useLocalStorage = (fn?: () => void) => {
+    const items = getLocalStorageItems()
+
+    const [storage, setStorage] = useState<{ [x: string]: string | null }[]>(items)
+
+    useEffect(() => {
+        if (fn && typeof fn == 'function') {
+            fn()
+        }
+    }, [storage])
+
+    const getValue = (itemKey: string) => {
+        const items = Object.values(storage)
+
+        for (const item of items) {
+            if (itemKey in item) {
+                return item[itemKey]
+            }
+        }
+
+        return null
+    }
+
+    const getMultipleValues = (keys: string[]) => {
+        const multipleValues: string[] = []
+
+        keys.forEach(key => {
+            const value = getValue(key) as string
+            multipleValues.push(value)
+        })
+
+        return multipleValues
+    }
+
+    const addItem = (key: string, value: string) => {
+        const item = checkItem(key)
+
+        if (!item) {
+            localStorage.setItem(key, value)
+            setStorage([...storage, { [key]: value }])
+        }
+    }
+
+    const addMultipleItems = (items: { key: string; value: string }[]) => {
+        for (const item of items) {
+            addItem(item.key, item.value)
+        }
+    }
+
+    const deleteItem = (key: string) => {
+        const item = checkItem(key)
+        if (!item) {
+            throw new Error('This item does not exist in the local storage')
+        }
+
+        if (item) {
+            localStorage.removeItem(key)
+            setStorage(getLocalStorageItems())
+        }
+    }
+
+    const deleteMultipleItems = (keys: string[]) => {
+        keys.forEach(key => {
+            const item = checkItem(key)
+            if (item) {
+                localStorage.removeItem(key)
+                setStorage(getLocalStorageItems())
+            }
+        })
+    }
+
+    return { storage, getValue, getMultipleValues, addItem, addMultipleItems, deleteItem, deleteMultipleItems }
+}
+
+export const checkItem = (key: string) => localStorage.getItem(key)
+
+export const getLocalStorageItems = () => {
+    const items: { [x: string]: string | null }[] = []
+
+    const keys = Object.keys(localStorage)
+
+    keys.forEach(key => {
+        items.push({ [key]: localStorage.getItem(key) })
+    })
+
+    return items
+}
---
+++ src/hooks/ui/__tests__/useLoading.spec.ts
@@ -0,0 +1,22 @@
+import { useLoading } from '../useLoading'
+import { renderHook } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+
+describe('useLoading', () => {
+    it('should return false when it called first time', () => {
+        const { result } = renderHook(() => useLoading(false))
+        expect(result.current.loading).toBeFalsy()
+    })
+
+    it('should return true when loading starts', () => {
+        const { result } = renderHook(() => useLoading(false))
+        act(() => result.current.startLoading())
+        expect(result.current.loading).toBeTruthy()
+    })
+
+    it('should return false when loading stops', () => {
+        const { result } = renderHook(() => useLoading(false))
+        act(() => result.current.stopLoading())
+        expect(result.current.loading).toBeFalsy()
+    })
+})
