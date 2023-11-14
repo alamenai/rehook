ca9dfd4afb70ab933d80bfc56ae1b6f5965e1215
Ala Eddine <ala.official.acc@gmail.com>
2023-11-14 21:52:17 +0100

    feat: implement useMouse hook

--- src/hooks/ui/useMouse.ts
+++ src/hooks/ui/useMouse.ts
@@ -1,1 +1,28 @@
-export const useMouse = () => {}
+import { useEffect, useState } from 'react'
+
+type Event = 'mouseenter' | 'mouseleave' | 'mouseover' | 'mousemove' | 'mouseout' | 'click'
+
+export const useMouse = (mouseEvent: Event, fn?: () => void) => {
+    const [mousePoisition, setMousePosition] = useState<{ x: number | undefined; y: number | undefined }>({
+        x: undefined,
+        y: undefined,
+    })
+
+    useEffect(() => {
+        const handleMouseEvent = (event: MouseEvent) => {
+            setMousePosition({ x: event.clientX, y: event.clientY })
+
+            if (fn) {
+                fn()
+            }
+        }
+
+        window.addEventListener(mouseEvent, handleMouseEvent)
+
+        return () => {
+            window.removeEventListener(mouseEvent, handleMouseEvent)
+        }
+    }, [mousePoisition])
+
+    return mousePoisition
+}
