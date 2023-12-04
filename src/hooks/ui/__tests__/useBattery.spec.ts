aa1cbf13dcc76445878ba441357cfc23e8ee6142
Menai Ala Eddine <ala.official.acc@gmail.com>
2023-12-04 20:10:26 +0100

    feat: implement useBattery hook for Battery API (#8)

---
+++ src/hooks/ui/__tests__/useBattery.spec.ts
@@ -0,0 +1,14 @@
+import { useBattery } from '../useBattery'
+import { renderHook } from '@testing-library/react'
+
+describe('useBattery', () => {
+    it('should return the level and charging status of the battery', async () => {
+        if ('getBattery' in navigator) {
+            /* eslint-disable  @typescript-eslint/no-explicit-any */
+            const battery = await (navigator as any).getBattery()
+            const { result } = renderHook(() => useBattery())
+            expect(result.current?.level).toBe(battery.level)
+            expect(result.current?.charging).toBe(battery.charging)
+        }
+    })
+})
