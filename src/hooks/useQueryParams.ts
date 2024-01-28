90e8b785550cb5765694e42130aa311df8906d8c
Ala Eddine <ala.official.acc@gmail.com>
2024-01-28 17:45:03 +0100

    feat: implement useParams hook for interacting with query string

---
+++ src/hooks/useQueryParams.ts
@@ -0,0 +1,41 @@
+import { useHistory } from './useHistory'
+
+export const useQueryParams = () => {
+    const history = useHistory()
+
+    const urlParams = new URLSearchParams(window.location.search)
+
+    const get = (param: string) => {
+        for (const [key, value] of urlParams) {
+            if (param === key) {
+                return value
+            }
+        }
+    }
+
+    const getParams = () => {
+        const keys = []
+
+        for (const [key] of urlParams) {
+            keys.push(key)
+        }
+
+        return keys
+    }
+
+    const set = (name: string, value: string) => {
+        const isFound = getParams().some(key => key === name)
+
+        if (isFound) {
+            urlParams.set(name, value)
+        } else {
+            urlParams.append(name, value)
+        }
+
+        const newRelativePathQuery = window.location.pathname + '?' + urlParams.toString()
+
+        history.push(newRelativePathQuery)
+    }
+
+    return { urlParams, get, getParams, set }
+}
