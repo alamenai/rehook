2fc978e04b2c05f7e73509e9af66cc1f7ffa06fb
Naoui BRIHMAT <brihmat.naoui@gmail.com>
2024-05-14 23:04:24 +0100

    linting and testing our code before merging a pull request

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import './App.css';
-import reactLogo from './assets/react.svg';
+import { useQueryParams } from './hooks/useQueryParams';
+// import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import { useState } from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
+    const { get, set } = useQueryParams();
+    const handleClick = () => {
+        set('page', '2');
+      };
 
     return (
         <>
             <div>
+                <p>Current page: {get('page') || 'N/A'}</p>
+                <button onClick={handleClick}>Go to page 2</button>
+             </div>
+            <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                    {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
                 </a>
                 <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
                 </a>
             </div>
             <h1>Vite + React</h1>
