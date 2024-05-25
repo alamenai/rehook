fdfcad05572f93a79f0667c0e807c3c51bd0950a
Ala <ala.menai@credium.de>
2024-05-25 15:39:25 +0100

    feat: implement usePersistent hook for synchronizing a state with local storage

---
+++ src/hooks/use-persistent-state.ts
@@ -0,0 +1,27 @@
+import { Item, useLocalStorage } from './use-local-storage'
+import { useEffect, useState } from 'react'
+
+export const usePersistentState = <PesistentState>(
+    key: string,
+    initialState: PesistentState,
+): [PesistentState, React.Dispatch<React.SetStateAction<PesistentState>>] => {
+    const gettItem = () => {
+        const item = localStorage.getItem(key)
+
+        if (item) {
+            return item as PesistentState
+        }
+
+        return initialState
+    }
+
+    const [state, setState] = useState(gettItem())
+
+    const { addItem } = useLocalStorage()
+
+    useEffect(() => {
+        addItem(key ?? 'persistent-state', state as Item)
+    }, [state, key])
+
+    return [state, setState]
+}
