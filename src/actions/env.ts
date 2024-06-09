cb773aa264306f50fd07ae62415a42449331e722
Ala <ala.menai@credium.de>
2024-06-09 20:02:30 +0100

    refactor: change rehook config file

--- src/actions/env.ts
+++ src/actions/env.ts
@@ -1,17 +1,16 @@
 'use server'
 
-import { NEXT_DEFAULT_ENV_VARS, VITE_DEFAULT_ENV_VARS } from '../constants/env'
-import { rehookConfig } from '../rehook.config'
+import { NEXT_DEFAULT_ENV_VARS, VITE_DEFAULT_ENV_VARS, rehookConfig } from '../../rehook.config'
 
 const { ignoreDefaultEnv, framework } = rehookConfig
 
 const isFrameworkVite = framework === 'VITE' // Otherwise it is NEXTJS
 
 export const getEnv = async () => {
-    const variables = Object.keys(isFrameworkVite ? import.meta.env : process.env)
+    const variables = isFrameworkVite ? import.meta.env : process.env
 
     if (ignoreDefaultEnv) {
-        const filteredVariables = variables.filter(variable =>
+        const filteredVariables = variables.filter((variable: string) =>
             isFrameworkVite ? !VITE_DEFAULT_ENV_VARS.includes(variable) : !NEXT_DEFAULT_ENV_VARS.includes(variable),
         )
         return filteredVariables
--- src/constants/env.ts
+++
@@ -1,3 +0,0 @@
-export const VITE_DEFAULT_ENV_VARS = ['BASE_URL', 'NODE', 'MODE', 'DEV', 'PROD', 'SSR']
-
-export const NEXT_DEFAULT_ENV_VARS = ['NODE_ENV', 'TZ']
